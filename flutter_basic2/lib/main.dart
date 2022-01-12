2061fd4d14faf5e434b1b7d091e16107902ac83d
espeniel76 <espeniel69@gmail.com>
2022-01-12 10:31:18 +0900

    aaa

--- flutter_basic2/lib/main.dart
+++ flutter_basic2/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'cupertino_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -33,6 +34,7 @@ class HelloPage extends StatefulWidget {
 class _HelloPageState extends State<HelloPage> {
   String _message = 'Hello World';
   int _count = 0;
+  bool _is_bool = false;
 
   @override
   Widget build(BuildContext context) {
@@ -50,6 +52,15 @@ class _HelloPageState extends State<HelloPage> {
           children: [
             Text(_message, style: TextStyle(fontSize: 30)),
             Text('$_count', style: TextStyle(fontSize: 30)),
+            RaisedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => CupertinoPage()),
+                );
+              },
+              child: Text('화면 이동'),
+            )
           ],
         ),
       ),
@@ -58,7 +69,14 @@ class _HelloPageState extends State<HelloPage> {
 
   void _changeMessage() {
     setState(() {
-      _message = '헬로 월드';
+      if (!_is_bool) {
+        _message = '헬로 월드';
+        _is_bool = true;
+      } else {
+        _message = 'Hello World';
+        _is_bool = false;
+      }
+
       _count++;
     });
   }
---
+++ instagram_clone/lib/tab_page.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class TabPage extends StatefulWidget {
+  const TabPage({Key? key}) : super(key: key);
+
+  @override
+  _TabPageState createState() => _TabPageState();
+}
+
+class _TabPageState extends State<TabPage> {
+  int _selectedIndex = 0;
+  List _pages = [
+    Text('page1'),
+    Text('page2'),
+    Text('page3'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: _pages[_selectedIndex]),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _onItemTapped,
+        currentIndex: _selectedIndex,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search"),
+          BottomNavigationBarItem(icon: Icon(Icons.account_circle), label: "Account"),
+        ],
+      ),
+    );
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+}
